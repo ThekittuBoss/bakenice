3e51760a29f7127571603fa7f734f69cd99ceab5
Ashish <ashishkatiya49@gmail.com>
2023-02-12 17:25:50 +0530

    <commit-message>

--- bakenice/urls.py
+++ bakenice/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home , name='home'),
+    path('contact/',views.contact_form, name='contact'),
+    path('success/', views.success_view, name='success'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def contact(request):
+    return render(request, "contact.html")    
+
+
+
+
+def contact_form(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        if name and email and message:
+            Contact.objects.create(
+                name=name,
+                email=email,
+                subject=subject,
+                message=message,
+            )
+            return redirect('success')
+    return render(request, 'contact.html')
+
+
+def success_view(request):
+    return render(request, 'success.html')
